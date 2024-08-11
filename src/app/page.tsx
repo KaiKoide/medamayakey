a520ea36864cabcefe656b8e7f2fee7b2271d47e
Kai <kkai.chien0813@gmail.com>
2024-08-11 14:59:45 -0700

    feat: make responsive design on top page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,18 @@ export default function Home() {
 	return (
 		<>
 			<main>
-				<div className='sm:flex w-full home-h'>
+				<div className='relative md:static flex w-full home-h'>
 					<Image
 						src={HomeImage}
-						className='object-cover w-12/12 sm:w-6/12'
+						className='object-cover w-full md:w-1/2'
 						alt='Medamayakey HomeImage'
 					/>
 					<Image
 						src={LogoImage}
 						alt='Medamayakey Logo'
-						width={450}
-						height={450}
+						width={400}
+						height={400}
+						className='block absolute md:static top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 md:inset-0 md:translate-x-0 md:translate-y-0'
 					/>
 				</div>
 			</main>
