3e36b9f85ceab86f38571c50a2b0dae1c88d0cd6
Kai Koide <144519746+KaiKoide@users.noreply.github.com>
2024-08-29 14:18:54 -0700

    5 solve the errors (#6)
    * feat: remove side bar on the screen
    
    * chore: delete side bar and table components

--- src/app/myrecipes/page.tsx
+++ src/app/myrecipes/page.tsx
@@ -1,12 +1,10 @@
 import CartRecipes from '@/app/myrecipes/CartRecipes';
 import Cartbar from '@/components/Cartbar';
-import Sidebar from '@/components/Sidebar';
 
 export default function Myrecipes() {
 	return (
 		<>
 			<div className='flex flex-col lg:flex-row h-full w-full'>
-				<Sidebar />
 				<main className='flex-1 p-9 h-full w-full'>
 					<CartRecipes />
 				</main>
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,4 @@
 import RecipesFilter from '@/app/search/(filter)/RecipesFilter';
-import Sidebar from '@/components/Sidebar';
 import { RecipeResultProvider } from '@/contexts/recipeContext';
 import RecipesItem from './RecipesItem';
 
@@ -7,7 +6,6 @@ export default function RecipesSearch() {
 	return (
 		<>
 			<div className='flex flex-col lg:flex-row'>
-				<Sidebar />
 				<main className='p-9'>
 					<RecipeResultProvider>
 						<RecipesFilter />
